d2b32e3059ec9928e8f6b923caf6b0eb3a426cf3
wrisin <imwrisin@gmail.com>
2026-06-08 23:09:33 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# build script for armsoft project
+
+MODULE="cfddns"
+VERSION="1.1"
+TITLE="CloudFlare DDNS"
+DESCRIPTION="CloudFlare DDNS"
+HOME_URL="Module_cfddns.asp"
+TAGS="DDNS"
+AUTHOR="sadog"
+
+# Check and include base
+DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
+
+# now include build_base.sh
+. $DIR/../softcenter/build_base.sh
+
+# change to module directory
+cd $DIR
+
+# do something here
+do_build_result
---
+++ config.json.js
@@ -0,0 +1,12 @@
+{
+"version":"1.1",
+"md5":"ef14adb89c43338a50e4c970b5345050",
+"home_url":"Module_cfddns.asp",
+"title":"CloudFlare DDNS",
+"description":"CloudFlare DDNS",
+"tags":"DDNS",
+"author":"sadog",
+"link":"",
+"changelog":"",
+"build_date":"2021-11-24_11:03:45"
+}
